69dee91e74ea958c8a4c9d363955e1454a8efbf9
Bowser-ai <martijnvermeulen1@gmail.com>
2021-05-07 17:17:14 +0200

    added some forms

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordChangeDoneView, PasswordResetConfirmView, \
+    PasswordResetCompleteView
 from django.urls import path
 
 from account.views import Dashboard, CustomLogoutView
@@ -8,4 +9,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
+    path('password_change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('reset/<uid64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
